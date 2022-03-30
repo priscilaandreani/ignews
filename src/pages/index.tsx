e6f2fe0cbcbd0624bb8c84fa961d142ceec1e7d4
priscila andreani <priscilaandreani@gmail.com>
2022-03-30 16:59:21 -0300

    feat: add Document and unique title for each page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello<span>world</span>
-    </h1>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <h1 className={styles.title}>
+        Hello <span>world</span>
+      </h1>
+    </>
   );
 }
